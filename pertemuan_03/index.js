c74b02f09d193e23c923abefcd5a468f75b3f3b5
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-08-29 22:59:32 +0800

    2nd commit

---
+++ pertemuan_03/index.js
@@ -0,0 +1,14 @@
+/*
+    DASAR DASAR PEMOGRAMAN JAVASCRIPT
+    1. Variabel dua tipe dara
+    2. Operator
+    3. Array
+    4. Object
+    5. Fungsi
+    6. JavaScript Conditional
+    7. JavaScript Loop
+*/
+//================================================
+
+
+console.log("Hello World, from external file");
