cdd20b4ae73e23b5caf36e1c4e8a05f98de03cf6
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-09-07 13:14:14 +0800

    4th commit

--- pertemuan_04/index.js
+++ pertemuan_04/index.js
@@ -4,13 +4,16 @@
 
 function penjumlahanDuaBujurSangkar(a, b){
 
-let LuasA = a * a;
-let LuasB = b * b;
-
-let total = LuasA + LuasB;
-
-return total;
+return a * a + b * b;
 }
 
  console.log(penjumlahanDuaBujurSangkar(8, 3));
  console.log(penjumlahanDuaBujurSangkar(10, 5));
+ 
+ function tambah(a, b){
+     return a + b;
+ }
+function kali(a, b){
+    return a * b;
+}
+console.log(tambah(kali(5 * 2),kali ( 10 * 2 )));
