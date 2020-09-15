6246600c9ffd615d654bc0e9ce40d22fabf22a46
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-09-15 20:16:46 +0800

    8th commit

---
+++ pertemuan_07/index.js
@@ -0,0 +1,97 @@
+//Menggunakan if, else if, dan else
+//let nilaiAndi = 95;
+
+//if(nilaiAndi > 80){
+//    console.log("SANGAT MEMUASKAN")
+//} 
+//else if (nilaiAndi >= 60 && nilaiAndi <= 80){
+//    console.log("MEMUASKAN")
+//}
+//else{
+//    console.log("JANGAN MENYERAH, COBA LAGI");
+//}
+//Menggunakan switch dan case
+//let seragamSekolah;
+//let hari = "senin";
+
+//switch (hari){
+//    case "senin":
+//        seragamSekolah = "kemeja putih bawahan merah";
+//        break;
+//    case "selasa":
+//        seragamSekolah = "kemeja hijau bawahan hitam";
+//        break;
+//    case "rabu":
+//        seragamSekolah = "kemeja putih bawahan putih";
+//        break;
+//    case "kamis":
+//        seragamSekolah = "kemeja batik merah bawahan hitam";
+//        break;
+//    case "jumat":
+//        seragamSekolah = "baju pramuka";
+//        break;
+//    default:
+//        serangamSekolah = "baju bebas"
+//}
+// console.log(seragamSekolah);
+
+
+//let seragamSekolah;
+//let hari = "senin";
+
+//switch (hari){
+//    case "senin":
+//    case "selasa":
+//    case "rabu":
+//    case "kamis":
+//        seragamSekolah = "kemeja putih bawahan merah";
+//        break;
+//    case "jumat":
+//        seragamSekolah = "baju pramuka";
+//        break;
+//    default:
+//        seragamSekolah = "baju bebas"
+//}
+//console.log(seragamSekolah);
+
+
+//For Loop
+//for (let i = 1; i <= 10; i++){
+//    consolw.log(i);
+//}
+
+
+//For/inLoop
+//conts buku = {
+//    judul: "Harry Potter and The Phillosopher's Stone",
+//    pengarang: "J.K. Rowling",
+//    tahun: 1997
+//};
+//for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//}
+
+
+//For/ofLoop
+//conts buku = ["Game of Thrones: A song of Ice and Fire",
+//"Harry Potter and The Philosopher's Stone",
+//"Lord of The Rings: The Fellowship of The Ring"
+//];
+//for (x of buku) {
+//    console.log(x);
+//}
+
+//While Loop
+//let i = 1;
+//while (1 <= 10){
+//    console.log(i);
+//    i++
+//}
+
+//Do while Loop
+let i = 1;
+
+do {
+    console.log(i);
+    i++
+} while (i <= 10);
