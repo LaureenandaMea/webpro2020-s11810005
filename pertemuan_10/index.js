3fac08e7274992072a5edeead30e507f10fe5923
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-09-24 00:05:22 +0800

    10th commit

---
+++ pertemuan_10/index.js
@@ -0,0 +1,17 @@
+//var, Let, Conts
+
+//let nama = "lauree";
+//let nama = "mea";
+
+//console.log(nama);
+
+//Scope dalam javascript
+//1. Function script (var)
+//2. Block scope
+
+(function tes(){
+    for (var i = 0; i < 0; i++){
+        console.log(i)
+    }
+})();
+console.log(i);
