ba6e8236b872259f8a33cfd909aba645e2cb67cc
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-09-24 13:39:48 +0800

    11th commit

--- pertemuan_10/index.js
+++ pertemuan_10/index.js
@@ -9,9 +9,40 @@
 //1. Function script (var)
 //2. Block scope
 
-(function tes(){
-    for (var i = 0; i < 0; i++){
-        console.log(i)
-    }
-})();
-console.log(i);
+// (function tes(){
+//   for (var i = 0; i < 0; i++){
+//       console.log(i)
+//   }
+// })();
+// console.log(i);
+
+//{
+//    let i;
+//    for (let i=0; i<0; i++){
+//        console.log(i)
+//    }
+//}
+//console.log(i);
+
+
+//Template Literal
+
+conts person = {
+    firstName: "Meldiva",
+    lastName: "Mea",
+    age: 19,
+};
+
+console.log(
+    "Hello nama saya" +
+     person.firstName +
+     " " + 
+     person.lastName +
+     ". Umur saya adalah " + 
+     person.age + 
+     " tahun."
+     );
+
+console.log(
+    `Hallo nama saya ${person.firstName} ${apersmon.lastName}. Umur saya adalah ${person.age}. `
+);
---
+++ pertemuan_11/index.js
@@ -0,0 +1,38 @@
+/*
+    Arrow function
+*/
+
+// function ucapkanSalam(){
+//    return "Selamat Pagi";
+//};
+
+// fungsi dengan parameter
+// const operasiPenjumlahan = (bilangan1, bilangan2) => {
+//    const hasil =  bilangan1 + bilangan2;
+//    return hasil;
+//  };
+  
+//  console.log(operasiPenjumlahan(3, 4)); // Output: 7
+  
+  // fungsi yang tidak memiliki parameter
+//  const namaJenisAnjing = () => {
+//    const anjing = ["Pug", "Bulldog", "Poodle"];
+//    return anjing[Math.floor(Math.random()*(anjing.length))];
+//  }
+  
+//  console.log(namaJenisAnjing()); // Output: Pug (hasil random)
+
+
+// Implicit return value
+//function greeting(nama) {
+//    return `Hay ${nama}`;
+// }
+ 
+// console.log(greeting('Laureenanda')); // Output: Hay Laureenanda
+
+ //cara singkat
+ const panggilan = (nama) => `Hay ${nama}`;
+
+console.log(panggilan('Laureenanda')); // Output: Hay Laureenanda
+
+  
