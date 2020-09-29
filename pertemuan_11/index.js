ab09226c6a93b3b5789b72a30b67928dbef4254e
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-09-29 18:15:21 +0800

    11th commit

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -24,15 +24,40 @@
 
 
 // Implicit return value
-//function greeting(nama) {
+//function panggilan(nama) {
 //    return `Hay ${nama}`;
 // }
  
-// console.log(greeting('Laureenanda')); // Output: Hay Laureenanda
+// console.log(panggilan('Laureenanda')); // Output: Hay Laureenanda
 
  //cara singkat
- const panggilan = (nama) => `Hay ${nama}`;
+ // const panggilan = (nama) => `Hay ${nama}`;
+// console.log(panggilan('Laureenanda')); // Output: Hay Laureenanda
 
-console.log(panggilan('Laureenanda')); // Output: Hay Laureenanda
+  // exercise
+  // Convert to ES6 syntax
+  // function yearUntilRetirement(year, firstName) {
+  //  var age = 28;
+  //    va retirement = 65 - age;
+  //    if(retirement > 0) {
+  //    console.log(firstName + " retired in " + retirement + "years");
+  //  } else {
+  //    console.log(firstName + " is alreadu retired. ");
+  //  }
+  // } 
 
-  
+  //ES6
+  // Exercise 3
+
+  const yearUntilRetirement = (year, firstName) => {
+    let age = 28;
+    const retirement = 65 - age;
+    if(retirement > 0){
+        console.log(`${firstName} will retire in ${retirement} years.`);
+    }
+    else{
+        console.log(`${firstName} has already retired.`);
+    }
+}
+
+console.log(yearUntilRetirement(2020, "laureen"));
