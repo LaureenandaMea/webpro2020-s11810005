7d08b3d216407412f442eb8729cb95491f69b8d0
LaureenandaMea <s11810005@student.unklab.ac.id>
2020-11-12 08:20:22 +0800

    22th commit

---
+++ pertemuan_20/src/Utils/Person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Laureenanda Mea",
+        institution: "Unklab",
+        address: "Tondano",
+        phone: "089605487006",
+    },
+    {
+        name: "Levin",
+        institution: "Unklab",
+        address: "China",
+        phone: "0811xxxxxxx",
+    },
+    {
+        name: "Melidva",
+        institution: "Unsrat",
+        address: "Manado",
+        phone: "0898xxxxxx",
+    },
+    {
+        name: "Lavina",
+        institution: "UPH",
+        address: "Jakarta",
+        phone: "0821xxxxxx",
+    },
+    ];
